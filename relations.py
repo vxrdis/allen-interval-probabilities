94f9061a1e4a3c24b90832ca9062ae6082608a47
Cillín Forrester <cforrest@tcd.ie>
2025-04-06 13:16:02 +0100

    Refactor relations.py for improved readability and efficiency; streamline functions and enhance vocabulary handling

--- relations.py
+++ relations.py
@@ -2,157 +2,120 @@ import constants as c
 
 
 def l(x):
-    return "l" + str(x)
+    return f"l{x}"
 
 
 def r(x):
-    return "r" + str(x)
+    return f"r{x}"
 
 
-def voc(string):
-    if len(string) == 0:
-        return {}
-    else:
-        return string[0].union(voc(string[1:]))
+# Get the vocabulary (set of all endpoints) from a symbolic sequence
+def voc(seq):
+    return set().union(*seq) if seq else set()
 
 
-def superpose(string1, string2, voc1, voc2):
-    if len(string1) == len(string2) == 0:
-        return [string1]
-    else:
-        temp = []
-        if len(string1) > 0:
-            h1 = string1[0]
-            t1 = string1[1:]
-            h1v2 = h1.intersection(voc2)
-            if len(h1v2) == 0:
-                for res in superpose(t1, string2, voc1, voc2):
-                    temp.append([h1] + res)
-            if len(string2) > 0:
-                h2 = string2[0]
-                t2 = string2[1:]
-                h3 = h1.union(h2)
-                if h1v2.issubset(h2):
-                    h2v1 = h2.intersection(voc1)
-                    if h2v1.issubset(h1):
-                        for res in superpose(t1, t2, voc1, voc2):
-                            temp.append([h3] + res)
-        if len(string2) > 0:
-            h2 = string2[0]
-            t2 = string2[1:]
-            h2v1 = h2.intersection(voc1)
-            if len(h2v1) == 0:
-                for res in superpose(string1, t2, voc1, voc2):
-                    temp.append([h2] + res)
-        return temp
+# Superpose two symbolic strings if vocabulary constraints allow
+def superpose(s1, s2, v1, v2):
+    if not s1 and not s2:
+        return [[]]
+
+    results = []
+    if s1:
+        h1, t1 = s1[0], s1[1:]
+        if not h1 & v2:
+            for rest in superpose(t1, s2, v1, v2):
+                results.append([h1] + rest)
+        elif s2:
+            h2, t2 = s2[0], s2[1:]
+            merged = h1 | h2
+            if h1 & v2 <= h2 and h2 & v1 <= h1:
+                for rest in superpose(t1, t2, v1, v2):
+                    results.append([merged] + rest)
+
+    if s2:
+        h2, t2 = s2[0], s2[1:]
+        if not h2 & v1:
+            for rest in superpose(s1, t2, v1, v2):
+                results.append([h2] + rest)
+
+    return results
 
 
 def super(s1, s2):
     return superpose(s1, s2, voc(s1), voc(s2))
 
 
-def proj(string, set):
-    if len(string) == 0:
-        return string
-    else:
-        h1 = string[0].intersection(set)
-        if len(h1) == 0:
-            return proj(string[1:], set)
-        else:
-            return [h1] + proj(string[1:], set)
+# Project a symbolic sequence to a set of endpoints
+def proj(seq, keep):
+    return [s & keep for s in seq if s & keep]
 
 
+# Return symbolic string representation for a given Allen relation
 def allen(x, y, rel):
-    lx = l(x)
-    rx = r(x)
-    ly = l(y)
-    ry = r(y)
-    if rel == c.BEFORE:
-        return [{lx}, {rx}, {ly}, {ry}]
-    elif rel == c.OVERLAPS:
-        return [{lx}, {ly}, {rx}, {ry}]
-    elif rel == c.MEETS:
-        return [{lx}, {rx, ly}, {ry}]
-    elif rel == c.DURING:
-        return [{ly}, {lx}, {rx}, {ry}]
-    elif rel == c.STARTS:
-        return [{lx, ly}, {rx}, {ry}]
-    elif rel == c.FINISHES:
-        return [{ly}, {lx}, {rx, ry}]
-    if rel == c.AFTER:
-        return [{ly}, {ry}, {lx}, {rx}]
-    elif rel == c.OVERLAPPED_BY:
-        return [{ly}, {lx}, {ry}, {rx}]
-    elif rel == c.MET_BY:
-        return [{ly}, {ry, lx}, {rx}]
-    elif rel == c.CONTAINS:
-        return [{lx}, {ly}, {ry}, {rx}]
-    elif rel == c.STARTED_BY:
-        return [{lx, ly}, {ry}, {rx}]
-    elif rel == c.FINISHED_BY:
-        return [{lx}, {ly}, {rx, ry}]
-    elif rel == c.EQUALS:
-        return [{lx, ly}, {rx, ry}]
+    lx, rx, ly, ry = l(x), r(x), l(y), r(y)
+    return {
+        c.BEFORE: [{lx}, {rx}, {ly}, {ry}],
+        c.MEETS: [{lx}, {rx, ly}, {ry}],
+        c.OVERLAPS: [{lx}, {ly}, {rx}, {ry}],
+        c.DURING: [{ly}, {lx}, {rx}, {ry}],
+        c.STARTS: [{lx, ly}, {rx}, {ry}],
+        c.FINISHES: [{ly}, {lx}, {rx, ry}],
+        c.AFTER: [{ly}, {ry}, {lx}, {rx}],
+        c.MET_BY: [{ly}, {ry, lx}, {rx}],
+        c.OVERLAPPED_BY: [{ly}, {lx}, {ry}, {rx}],
+        c.CONTAINS: [{lx}, {ly}, {ry}, {rx}],
+        c.STARTED_BY: [{lx, ly}, {ry}, {rx}],
+        c.FINISHED_BY: [{lx}, {ly}, {rx, ry}],
+        c.EQUALS: [{lx, ly}, {rx, ry}],
+    }.get(rel)
 
 
+# Return Allen relation name from a symbolic sequence
 def allInv(string, x, y):
-    lx = l(x)
-    rx = r(x)
-    ly = l(y)
-    ry = r(y)
-    if string == [{lx}, {rx}, {ly}, {ry}]:
-        return "p"
-    elif string == [{lx}, {ly}, {rx}, {ry}]:
-        return "o"
-    elif string == [{lx}, {rx, ly}, {ry}]:
-        return "m"
-    elif string == [{ly}, {lx}, {rx}, {ry}]:
-        return "d"
-    elif string == [{lx, ly}, {rx}, {ry}]:
-        return "s"
-    elif string == [{ly}, {lx}, {rx, ry}]:
-        return "f"
-    elif string == [{ly}, {ry}, {lx}, {rx}]:
-        return "P"
-    elif string == [{ly}, {lx}, {ry}, {rx}]:
-        return "O"
-    elif string == [{ly}, {ry, lx}, {rx}]:
-        return "M"
-    elif string == [{lx}, {ly}, {ry}, {rx}]:
-        return "D"
-    elif string == [{lx, ly}, {ry}, {rx}]:
-        return "S"
-    elif string == [{lx}, {ly}, {rx, ry}]:
-        return "F"
-    elif string == [{lx, ly}, {rx, ry}]:
-        return "e"
+    lx, rx, ly, ry = l(x), r(x), l(y), r(y)
+    patterns = {
+        "p": [{lx}, {rx}, {ly}, {ry}],
+        "m": [{lx}, {rx, ly}, {ry}],
+        "o": [{lx}, {ly}, {rx}, {ry}],
+        "d": [{ly}, {lx}, {rx}, {ry}],
+        "s": [{lx, ly}, {rx}, {ry}],
+        "f": [{ly}, {lx}, {rx, ry}],
+        "P": [{ly}, {ry}, {lx}, {rx}],
+        "M": [{ly}, {ry, lx}, {rx}],
+        "O": [{ly}, {lx}, {ry}, {rx}],
+        "D": [{lx}, {ly}, {ry}, {rx}],
+        "S": [{lx, ly}, {ry}, {rx}],
+        "F": [{lx}, {ly}, {rx, ry}],
+        "e": [{lx, ly}, {rx, ry}],
+    }
+    for rel, pattern in patterns.items():
+        if string == pattern:
+            return rel
+    return "?"
 
 
+# Compute composition r1 ∘ r2 using symbolic superposition
 def tt(r1, r2):
-    temp = []
-    for s in super(allen(0, 1, r1), allen(1, 2, r2)):
-        temp.append(allInv(proj(s, {l(0), r(0), l(2), r(2)}), 0, 2))
-    return temp
+    results = []
+    s1 = allen(0, 1, r1)
+    s2 = allen(1, 2, r2)
+    for merged in super(s1, s2):
+        proj_02 = proj(merged, {l(0), r(0), l(2), r(2)})
+        results.append(allInv(proj_02, 0, 2))
+    return results
 
 
+# Print visual debug of symbolic composition
 def show(r1, r2):
     s1 = allen(0, 1, r1)
     s2 = allen(1, 2, r2)
-    print(" ", s1, "(depicting 0", r1, "1) and")
-    print(" ", s2, "(depicting 1", r2, "2)")
-    for s in super(s1, s2):
-        print("   0", allInv(proj(s, {l(0), r(0), l(2), r(2)}), 0, 2), "2 from ", s)
-
-
-def test():
-    print("Allen relations from [{'l0'}, {'r0'}] superposed with [{'l1'}, {'r1'}]")
-    al = super([{"l0"}, {"r0"}], [{"l1"}, {"r1"}])
-    for s in al:
-        print(" ", allInv(s, 0, 1), s)
-    print("Allen transitivity table tt(r1,r2)")
-    print('e.g. tt("p",d") =', tt("p", "d"), "by superposing")
-    show("p", "d")
+    print(" ", s1, f"(depicting 0 {r1} 1)")
+    print(" ", s2, f"(depicting 1 {r2} 2)")
+    for merged in super(s1, s2):
+        rel = allInv(proj(merged, {l(0), r(0), l(2), r(2)}), 0, 2)
+        print("   0", rel, "2 from", merged)
 
 
 if __name__ == "__main__":
-    test()
+    print("Example: tt('p', 'd') =", tt("p", "d"))
+    show("p", "d")
